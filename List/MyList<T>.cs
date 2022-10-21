a0c415819dbf94d25e9565d8976a8937b9bf1713
Mykhailo <misha.vrana@gmail.com>
2022-10-21 14:53:45 +0300

    initial commit

---
+++ List/MyList<T>.cs
@@ -0,0 +1,18 @@
+namespace Lab_6;
+
+
+/*
+ * MyList<T> must implement such features:
+ *  - Append element
+ *  - Remove element
+ *  - Indexation mechanism
+ *  - Get element
+ *  - Get amount of elements in array
+ * 
+ */
+public class MyList<T>
+{
+ private T[] data;
+ private int size = 0;
+ private int capacity; 
+}
